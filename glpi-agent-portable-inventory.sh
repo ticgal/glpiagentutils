e533990863044cc2806d2f460a314e7aa09da03c
Oscar B <oscar@ticgal.com>
2024-01-22 20:25:15 +0100

    fix license
    Signed-off-by: Oscar B. <oscar@ticgal.com>

--- glpi-agent-portable-inventory.sh
+++ glpi-agent-portable-inventory.sh
@@ -16,7 +16,7 @@
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with Notifications plugin. If not, see <http://www.gnu.org/licenses/>.
+# along with glpiagentutils. If not, see <http://www.gnu.org/licenses/>.
 #
 # ------------------------------------------------------------------------
 #
